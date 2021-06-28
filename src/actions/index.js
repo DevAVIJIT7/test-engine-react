787836d2c4cb7b543035f4a69468097094e7facc
Avijit Majhi <avijit.majhi@bugcrowd.com>
2021-06-28 23:09:59 +0530

    eslist configuration, style fixes

--- src/actions/index.js
+++ src/actions/index.js
@@ -42,17 +42,17 @@ export const fetchUser = () => async (dispatch) => {
 };
 
 // export const createStream = (formValues) => {
-// 	return async (dispatch, getState) => {
-// 		const { userId } = getState().auth;
-// 		const response = await streams.post('/streams', {...formValues, userId})
+//   return async (dispatch, getState) => {
+//     const { userId } = getState().auth;
+//     const response = await streams.post('/streams', {...formValues, userId})
 
-// 		dispatch({
-// 			type: CREATE_STREAM,
-// 			payload: response.data
-// 		})
+//     dispatch({
+//       type: CREATE_STREAM,
+//       payload: response.data
+//     })
 
-// 		history.push('/')
-// 	}
+//     history.push('/')
+//   }
 // }
 
 export const fetchTests = () => async (dispatch) => {
@@ -65,36 +65,36 @@ export const fetchTests = () => async (dispatch) => {
 };
 
 // export const fetchStream = (id) => {
-// 	return async (dispatch) => {
-// 		const response = await streams.get(`/streams/${id}`)
+//   return async (dispatch) => {
+//     const response = await streams.get(`/streams/${id}`)
 
-// 		dispatch({
-// 			type: FETCH_STREAM,
-// 			payload: response.data
-// 		})
-// 	}
+//     dispatch({
+//       type: FETCH_STREAM,
+//       payload: response.data
+//     })
+//   }
 // }
 
 // export const editStream = (id, formValues) => {
-// 	return async (dispatch) => {
-// 		const response = await streams.patch(`/streams/${id}`, formValues)
+//   return async (dispatch) => {
+//     const response = await streams.patch(`/streams/${id}`, formValues)
 
-// 		dispatch({
-// 			type: EDIT_STREAM,
-// 			payload: response.data
-// 		})
+//     dispatch({
+//       type: EDIT_STREAM,
+//       payload: response.data
+//     })
 
-// 		history.push('/')
-// 	}
+//     history.push('/')
+//   }
 // }
 
 // export const deleteStream = (id) => {
-// 	return async (dispatch) => {
-// 		await streams.delete(`/streams/${id}`)
+//   return async (dispatch) => {
+//     await streams.delete(`/streams/${id}`)
 
-// 		dispatch({
-// 			type: DELETE_STREAM,
-// 			payload: id
-// 		})
-// 	}
+//     dispatch({
+//       type: DELETE_STREAM,
+//       payload: id
+//     })
+//   }
 // }
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -8,18 +8,19 @@ const INTIAL_STATE = {
 
 export default (state = INTIAL_STATE, action) => {
   switch (action.type) {
-    case SIGN_IN:
+    case SIGN_IN: {
       const { user } = action.payload;
       const token = localStorage.getItem('token');
       return {
         ...state, isSignedIn: true, user, token,
       };
+    }
     case SIGN_OUT:
       return { ...state, isSignedIn: false, userId: null };
     case SIGN_IN_ERROR:
       return { ...state, isSignedIn: true, signInErrors: action.payload.data.errors };
     case FETCH_USER:
-		  return { ...state, user: action.payload.data };
+      return { ...state, user: action.payload.data };
     default:
       return state;
   }
