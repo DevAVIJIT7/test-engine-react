f739fe0a5433ea1ff2ec3df5397e2c79351655d9
Avijit Majhi <avijit.majhi@bugcrowd.com>
2021-07-03 20:08:20 +0530

    nav icons, fontawesome icons

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { faLaughWink, faTachometerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faLaughWink, faTachometerAlt, faVial } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Sidebar extends React.Component {
@@ -33,16 +33,16 @@ class Sidebar extends React.Component {
         <li className="nav-item">
           <a
             className="nav-link collapsed"
-            href="test.html"
+            href="#"
             data-toggle="collapse"
             data-target="#collapseTwo"
-            aria-expanded="true"
+            aria-expanded="false"
             aria-controls="collapseTwo"
           >
-            <i className="fas fa-fw fa-cog" />
-            <span>Components</span>
+            <FontAwesomeIcon icon={faVial} className="fa-sm mx-2"/>
+            <span>Tests</span>
           </a>
-          <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div id="collapseTwo" className="collapse show" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div className="bg-white py-2 collapse-inner rounded">
               <h6 className="collapse-header">Custom Components:</h6>
               <a className="collapse-item" href="buttons.html">Buttons</a>
